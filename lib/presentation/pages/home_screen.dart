b27b959ebbb8b1e3d7a715e8045b713ff9c4d330
mohammed alsamaki <86600688+mohammedalsamki@users.noreply.github.com>
2024-02-10 13:09:36 +0300

    new

--- lib/presentation/pages/home_screen.dart
+++ lib/presentation/pages/home_screen.dart
@@ -9,14 +9,16 @@ import 'package:movie_app/presentation/pages/movies_list.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
-  final bckImage = 'https://w0.peakpx.com/wallpaper/732/875/HD-wallpaper-anonymous-black-cool-dark-guy-foux-hacker-scary-tech.jpg';
+  final bckImage =
+      'https://w0.peakpx.com/wallpaper/732/875/HD-wallpaper-anonymous-black-cool-dark-guy-foux-hacker-scary-tech.jpg';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: false,
-        elevation: 0,toolbarHeight: 100,
+        elevation: 0,
+        toolbarHeight: 100,
         backgroundColor: Colors.black,
         title: const Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -64,14 +66,14 @@ class HomeScreen extends StatelessWidget {
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: <Widget>[
                           ElevatedButton(
-                            onPressed: () {
-                            },
-                            child: Text('▶️ Learn Flutter with Flutter Guys'),
+                            onPressed: () {},
+                            child: Text('▶️ Learn Flutter with SDK'),
                             style: ElevatedButton.styleFrom(
                               primary: Colors.red,
                               onPrimary: Colors.white,
                               shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20), // Border radius
+                                borderRadius:
+                                    BorderRadius.circular(20), // Border radius
                               ),
                             ),
                           ),
@@ -81,43 +83,54 @@ class HomeScreen extends StatelessWidget {
                   ],
                 ),
               ),
-              SizedBox(height: 30,),
+              SizedBox(
+                height: 30,
+              ),
 
               // Trending Movies
-              Text('Trending Movies',
-                style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+              Text(
+                'Trending Movies',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
               ),
               BlocBuilder<TrendingMoviesBloc, TrendingMoviesState>(
-                builder: (context, state){
-                  if(state is TrendingMoviesLoading){
+                builder: (context, state) {
+                  if (state is TrendingMoviesLoading) {
                     return CircularProgressIndicator();
-                  } else if(state is TrendingMoviesLoaded){
+                  } else if (state is TrendingMoviesLoaded) {
                     return MoviesList(movies: state.movies);
-                  } else if (state is TrendingMoviesError){
+                  } else if (state is TrendingMoviesError) {
                     return Text(state.message);
                   }
                   return Container();
                 },
               ),
 
-              SizedBox(height: 20,),
+              SizedBox(
+                height: 20,
+              ),
               // Popular Movies
-              Text('Popular Movies',
-                style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+              Text(
+                'Popular Movies',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
               ),
               BlocBuilder<PopularMoviesBloc, PopularMoviesState>(
-                builder: (context, state){
-                  if(state is PopularMoviesLoading){
+                builder: (context, state) {
+                  if (state is PopularMoviesLoading) {
                     return CircularProgressIndicator();
-                  } else if(state is PopularMoviesLoaded){
+                  } else if (state is PopularMoviesLoaded) {
                     return MoviesList(movies: state.movies);
-                  } else if (state is PopularMoviesError){
+                  } else if (state is PopularMoviesError) {
                     return Text(state.message);
                   }
                   return Container();
                 },
               ),
-
             ],
           ),
         ),
